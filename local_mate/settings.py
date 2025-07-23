5938b39ce324c17984a31b54f009fe923cbe7d49
addico786 <adnandelhi2004@gmail.com>
2025-07-23 09:31:18 +0000

    initial commit

--- local_mate/settings.py
+++ local_mate/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'website',
 ]
 
+AUTHENTICATION_BACKENDS=[
+    'django.countrib.auth.backends.ModelBackend'
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
