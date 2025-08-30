b721cf61df0b2cfec5132ef9591e053329b91a64
addico786 <adnandelhi2004@gmail.com>
2025-08-30 06:39:48 +0530

    initial commit

--- infrastructure/infra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read -p "enter the file name" name 
-
-touch $name
--- infrastructure/variable.tf
+++
@@ -1,6 +0,0 @@
-variable "name" {
-  default     = "loclal_mate"
-  description = "Name of the EC2 instance"
-  type        = string
-  
-}
